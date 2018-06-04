9e10ca6f35d42f96c29f4c8f4c972e6200b0b1af
Peter Booker <mail@peterbooker.com>
2018-06-04 19:57:41 +0100

    add rough example of POST handler

--- internal/server/api_handlers.go
+++ internal/server/api_handlers.go
@@ -1,7 +1,11 @@
 package server
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
 
+// getSearchList ...
 func (s *Server) getSearchList() http.HandlerFunc {
 	type fightsResponse struct {
 		Fights []string
@@ -25,15 +29,39 @@ func (s *Server) getSearchList() http.HandlerFunc {
 	}
 }
 
+// getSearch ...
 func (s *Server) getSearch() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		fight := ""
 		writeResp(w, fight)
 	}
 }
+
+// createSearch ...
 func (s *Server) createSearch() http.HandlerFunc {
+	type createSearchRequest struct {
+		input  string
+		target string
+	}
+
+	type createSearchResponse struct {
+		status int
+		id     string
+		err    string
+	}
+
 	return func(w http.ResponseWriter, r *http.Request) {
-		fight := ""
-		writeResp(w, fight)
+		decoder := json.NewDecoder(r.Body)
+
+		var data createSearchRequest
+		err := decoder.Decode(&data)
+		if err != nil {
+			panic(err)
+		}
+
+		// Create Search...
+
+		var resp createSearchResponse
+		writeResp(w, resp)
 	}
 }
