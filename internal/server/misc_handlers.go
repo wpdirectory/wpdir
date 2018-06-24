453ccee32034f7351f9eec02552f734340a6cb09
Peter Booker <mail@peterbooker.com>
2018-06-24 21:04:51 +0100

    updated html

--- internal/server/misc_handlers.go
+++ internal/server/misc_handlers.go
@@ -4,12 +4,16 @@ import (
 	"net/http"
 )
 
+var (
+	html = []byte(`<!DOCTYPE html><html lang="en"><head><meta charset="utf-8"><meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no"><meta name="theme-color" content="#000000"><link rel="manifest" href="/static/assets/manifest.json"><link rel="shortcut icon" href="/static/assets/favicon.ico"><title>WordPress Directory Searcher - WPdirectory</title><link href="/static/assets/css/main.c5c03dde.css" rel="stylesheet"></head><body><noscript>You need to enable JavaScript to run this app.</noscript><div id="root"></div><script type="text/javascript" src="/static/assets/js/main.0325f607.js"></script></body></html>`)
+)
+
 func (s *Server) static() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/html")
 		w.Header().Set("Vary", "Accept-Encoding")
 
-		w.Write([]byte(`<!DOCTYPE html><html lang="en"><head><meta charset="utf-8"><meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no"><meta name="theme-color" content="#000000"><link rel="manifest" href="/static/assets/manifest.json"><link rel="shortcut icon" href="/static/assets/favicon.ico"><title>WordPress Directory Searcher - WPdirectory</title><link href="/static/assets/css/main.7717625e.css" rel="stylesheet"></head><body><noscript>You need to enable JavaScript to run this app.</noscript><div id="root"></div><script type="text/javascript" src="/static/assets/js/main.216285b4.js"></script></body></html>`))
+		w.Write(html)
 	}
 }
 
@@ -19,6 +23,6 @@ func (s *Server) notFound() http.HandlerFunc {
 		w.Header().Set("Vary", "Accept-Encoding")
 		w.WriteHeader(http.StatusNotFound)
 
-		w.Write([]byte(`<!DOCTYPE html><html lang="en"><head><meta charset="utf-8"><meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no"><meta name="theme-color" content="#000000"><link rel="manifest" href="/static/assets/manifest.json"><link rel="shortcut icon" href="/static/assets/favicon.ico"><title>WordPress Directory Searcher - WPdirectory</title><link href="/static/assets/css/main.7717625e.css" rel="stylesheet"></head><body><noscript>You need to enable JavaScript to run this app.</noscript><div id="root"></div><script type="text/javascript" src="/static/assets/js/main.216285b4.js"></script></body></html>`))
+		w.Write(html)
 	}
 }
