662cc2b99be6c9ebffa875ca443901867686ea35
Peter Booker <mail@peterbooker.com>
2018-06-28 10:32:41 +0100

    updated footer links

--- web/src/components/Footer.js
+++ web/src/components/Footer.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="footer cell shrink">
-      <div className="info">&copy; 2018 WPdirectory | Made with Love, Go and React by <a href="https://www.peterbooker.com" target="_blank" rel="noopener noreferrer">Peter Booker</a> | WPdir v0.1.0</div>
+      <div className="info">&copy; 2018 WP Directory - Made with Love, Go and React by <a href="https://www.peterbooker.com" target="_blank" rel="noopener noreferrer">Peter Booker</a> - <a href="https://github.com/wpdirectory/wpdir" target="_blank" rel="noopener noreferrer">WPdir v0.1.0</a></div>
     </footer>
   );
 };
