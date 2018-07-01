de4aa8a926b6ae9ba755035d7ed43a064b5ae840
Peter Booker <mail@peterbooker.com>
2018-07-01 16:02:10 +0100

    added package for new search queue implementation

---
+++ internal/search/queue/queue.go
@@ -0,0 +1,59 @@
+package queue
+
+import "sync"
+
+// Queue contains information about and helper funcs for the queue
+type Queue struct {
+	queue chan string
+	pos   map[string]int
+	sync.RWMutex
+}
+
+// New returns a Queue struct
+func New(len int) *Queue {
+	return &Queue{
+		queue: make(chan string, len),
+		pos:   make(map[string]int),
+	}
+}
+
+// Add pushes an ID onto the Queue
+func (q *Queue) Add(id string) {
+	q.queue <- id
+
+	q.Lock()
+	q.pos[id] = len(q.queue)
+	q.Unlock()
+}
+
+// Get returns an item from the queue
+// Blocks until available
+func (q *Queue) Get() string {
+	id := <-q.queue
+
+	q.Lock()
+	defer q.Unlock()
+	delete(q.pos, id)
+	q.decrementPos()
+
+	return id
+}
+
+// decrementPos decrements each ID by one.
+func (q *Queue) decrementPos() {
+	for id := range q.pos {
+		q.pos[id]--
+	}
+}
+
+// Pos returns the position of ID in the queue
+// returns -1 if ID not found.
+func (q *Queue) Pos(id string) int {
+	q.RLock()
+	defer q.RUnlock()
+	pos, ok := q.pos[id]
+	if !ok {
+		return -1
+	}
+	return pos
+}
---
+++ internal/search/queue/queue_test.go
@@ -0,0 +1,106 @@
+package queue
+
+import "testing"
+
+func TestAdd(t *testing.T) {
+	queue := &Queue{
+		queue: make(chan string, 10),
+		pos:   make(map[string]int),
+	}
+	ids := []string{
+		"01CH6CNP575QSN84B4YH1FRGYC",
+		"01CH5K5GW8BQJ1CSTM2B4EZ5SZ",
+		"01CH40V4G0DZ2GRNAQ3Z3FT85H",
+	}
+
+	for key, id := range ids {
+		queue.Add(id)
+		got := len(queue.queue)
+		want := key + 1
+		if got != want {
+			t.Errorf("Expected length %d got %d", want, got)
+		}
+	}
+}
+
+func TestGet(t *testing.T) {
+	queue := &Queue{
+		queue: make(chan string, 10),
+		pos:   make(map[string]int),
+	}
+	ids := []string{
+		"01CH6CNP575QSN84B4YH1FRGYC",
+		"01CH5K5GW8BQJ1CSTM2B4EZ5SZ",
+		"01CH40V4G0DZ2GRNAQ3Z3FT85H",
+	}
+
+	for _, id := range ids {
+		queue.Add(id)
+	}
+
+	for _, id := range ids {
+		got := queue.Get()
+		want := id
+		if got != want {
+			t.Errorf("Expected ID %s got %s", want, got)
+		}
+	}
+}
+
+func TestPos(t *testing.T) {
+	queue := &Queue{
+		queue: make(chan string, 10),
+		pos:   make(map[string]int),
+	}
+	ids := []string{
+		"01CH6CNP575QSN84B4YH1FRGYC",
+		"01CH5K5GW8BQJ1CSTM2B4EZ5SZ",
+		"01CH40V4G0DZ2GRNAQ3Z3FT85H",
+	}
+
+	for _, id := range ids {
+		queue.Add(id)
+	}
+
+	for key, id := range ids {
+		got := queue.Pos(id)
+		want := key + 1
+		if got != want {
+			t.Errorf("Expected position %d got %d", want, got)
+		}
+	}
+}
+
+func TestDecrementPos(t *testing.T) {
+	queue := &Queue{
+		queue: make(chan string, 10),
+		pos:   make(map[string]int),
+	}
+
+	ids := []string{
+		"01CH6CNP575QSN84B4YH1FRGYC",
+		"01CH5K5GW8BQJ1CSTM2B4EZ5SZ",
+		"01CH40V4G0DZ2GRNAQ3Z3FT85H",
+	}
+
+	for _, id := range ids {
+		queue.Add(id)
+	}
+
+	id := queue.Get()
+	got := queue.Pos(id)
+	want := -1
+	if got != want {
+		t.Errorf("Expected invalid key (%d) got %d", want, got)
+	}
+
+	ids = ids[1:]
+
+	for key, id := range ids {
+		got := queue.Pos(id)
+		want := key + 1
+		if got != want {
+			t.Errorf("Expected position %d got %d", want, got)
+		}
+	}
+}
