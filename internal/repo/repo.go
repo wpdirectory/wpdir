445312a836a528393652838c70caa6f969c1c00d
Peter Booker <mail@peterbooker.com>
2018-07-06 00:01:22 +0100

    Added wporg api and logger to Repo structs

--- internal/repo/repo.go
+++ internal/repo/repo.go
@@ -1,10 +1,21 @@
 package repo
 
 import (
+	"log"
+	"net"
+	"net/http"
+	"runtime"
+	"time"
+
 	"github.com/wpdirectory/wpdir/internal/config"
 	"github.com/wpdirectory/wpdir/internal/index"
 	"github.com/wpdirectory/wpdir/internal/plugin"
 	"github.com/wpdirectory/wpdir/internal/theme"
+	"github.com/wpdirectory/wporg"
+)
+
+var (
+	httpClient *http.Client
 )
 
 // Repo ...
@@ -19,6 +30,8 @@ type Repo interface {
 	UpdateIndex(idx *index.Index) error
 	UpdateList() error
 
+	load() error
+	save() error
 	LoadExisting()
 
 	QueueUpdate(slug string)
@@ -29,24 +42,39 @@ type Repo interface {
 }
 
 // New returns a new Repo
-func New(t string, c *config.Config) Repo {
+func New(t string, c *config.Config, l *log.Logger) Repo {
+	// Setup HTTP Client
+	opt := func(c *wporg.Client) {
+		c.HTTPClient = httpClient
+	}
+	api := wporg.NewClient(opt)
 	var repo Repo
 	switch t {
 	case "plugins":
 		repo = &PluginRepo{
-			Config:      c,
+			cfg:         c,
+			log:         l,
+			api:         api,
 			List:        make(map[string]*plugin.Plugin),
 			Revision:    0,
 			UpdateQueue: make(chan string, 100000),
 		}
 	case "themes":
 		repo = &ThemeRepo{
-			Config:      c,
+			cfg:         c,
+			log:         l,
+			api:         api,
 			List:        make(map[string]*theme.Theme),
 			Revision:    0,
 			UpdateQueue: make(chan string, 100000),
 		}
 	}
+	// Load Existing Data
+	err := repo.load()
+	if err != nil {
+		l.Printf("Repo (%s) has no existing data\n", t)
+	}
+
 	return repo
 }
 
@@ -66,4 +94,25 @@ type Extension interface {
 	LoadAPIData() error
 	Update() error
 	Save() error
+}
+
+func init() {
+	var netTransport = &http.Transport{
+		Proxy: http.ProxyFromEnvironment,
+		DialContext: (&net.Dialer{
+			Timeout:   30 * time.Second,
+			KeepAlive: 30 * time.Second,
+			DualStack: true,
+		}).DialContext,
+		MaxIdleConns:          100,
+		IdleConnTimeout:       90 * time.Second,
+		TLSHandshakeTimeout:   10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+		MaxIdleConnsPerHost:   runtime.GOMAXPROCS(0) + 1,
+	}
+
+	httpClient = &http.Client{
+		Timeout:   time.Second * time.Duration(120),
+		Transport: netTransport,
+	}
 }
