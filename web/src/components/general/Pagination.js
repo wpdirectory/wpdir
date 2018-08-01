59f6c61d3a1e3be85feb0de660e686c771b852a4
Peter Booker <mail@peterbooker.com>
2018-08-01 16:33:10 +0100

    add page count to pagination

--- web/src/components/general/Pagination.js
+++ web/src/components/general/Pagination.js
@@ -8,10 +8,12 @@ class Pagination extends Component {
     } = this.props
     let prev = ( currentPage <= 1 ) ? true : false
     let next = ( currentPage >= totalPages ) ? true : false
+    let pageText = 'Page ' + currentPage + '/' + Math.ceil(totalPages)
     return (
-	    <nav className="search-nav expanded button-group" aria-label="Pagination">
-        <button type="button" className="button primary" onClick={this.props.prevClick} disabled={prev}>Previous</button>
-        <button type="button" className="button primary" onClick={this.props.nextClick} disabled={next}>Next</button>
+	    <nav className="search-nav grid-x" aria-label="Pagination">
+        <div className="cell small-6 medium-4"><button type="button" className="button primary expanded" onClick={this.props.prevClick} disabled={prev}>Previous</button></div>
+        <div className="cell small-6 medium-4 show-for-medium"><div className="search-nav-page">{pageText}</div></div>
+        <div className="cell small-6 medium-4"><button type="button" className="button primary expanded" onClick={this.props.nextClick} disabled={next}>Next</button></div>
       </nav>
 	  )
   }
