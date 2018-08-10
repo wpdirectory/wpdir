05bb2fb6ac83eb8a5bf1e03f51dd92a9f0431050
Peter Booker <mail@peterbooker.com>
2018-08-10 15:03:39 +0100

    Add dynamic version to frontend

--- web/src/components/Footer.js
+++ web/src/components/Footer.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Config from '../utils/Config.js'
 
 const Footer = () => {
   return (
     <footer className="footer cell shrink">
-      <div className="info">&copy; 2018 WP Directory - Made with Love, Go and React by <a href="https://www.peterbooker.com" target="_blank" rel="noopener noreferrer">Peter Booker</a> - <a href="https://github.com/wpdirectory/wpdir" target="_blank" rel="noopener noreferrer">WPdir v1.0.0 beta</a></div>
+      <div className="info">&copy; 2018 WP Directory - Made with Love, Go and React by <a href="https://www.peterbooker.com" target="_blank" rel="noopener noreferrer">Peter Booker</a> - <a href="https://github.com/wpdirectory/wpdir" target="_blank" rel="noopener noreferrer">{ 'v' + Config.Version }</a></div>
     </footer>
   )
 }
--- web/src/utils/Config.js
+++ web/src/utils/Config.js
@@ -1,4 +1,5 @@
 let Config = {
+    Version: (window.config.Version === '%VERSION%') ? '1.0.0' : window.config.Version,
     Hostname: (window.config.Hostname === '%HOSTNAME%') ? 'https://wpdirectory.net' : window.config.Hostname,
     HTTP: {
         Timeout: (window.config.Timeout === '%TIMEOUT%') ? 5000 : window.config.Timeout,
