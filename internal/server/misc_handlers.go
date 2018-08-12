27ef01c41765e874744faa47b4797101f3f09b5a
Peter Booker <mail@peterbooker.com>
2018-08-12 11:27:00 +0100

    only load and process html file once

--- internal/server/misc_handlers.go
+++ internal/server/misc_handlers.go
@@ -12,61 +12,61 @@ import (
 // Need to embed SEO related info too
 
 func (s *Server) static() http.HandlerFunc {
+	file, err := data.Assets.Open("/index.html")
+	if err != nil {
+		s.Logger.Fatalf("Failed to open index.html: %s\n", err)
+	}
+	defer file.Close()
+
+	fileinfo, err := file.Stat()
+	if err != nil {
+		s.Logger.Fatalf("Failed to get file info index.html: %s\n", err)
+	}
+
+	filesize := fileinfo.Size()
+	buffer := make([]byte, filesize)
+
+	_, err = file.Read(buffer)
+	if err != nil && err != io.EOF {
+		s.Logger.Fatalf("Failed to read index.html: %s\n", err)
+	}
+
+	html := s.addConfig(buffer)
+
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/html")
 		w.Header().Set("Vary", "Accept-Encoding")
-
-		file, err := data.Assets.Open("/index.html")
-		if err != nil {
-			s.Logger.Fatalf("Failed to open index.html: %s\n", err)
-		}
-		defer file.Close()
-
-		fileinfo, err := file.Stat()
-		if err != nil {
-			s.Logger.Fatalf("Failed to get file info index.html: %s\n", err)
-		}
-
-		filesize := fileinfo.Size()
-		buffer := make([]byte, filesize)
-
-		_, err = file.Read(buffer)
-		if err != nil && err != io.EOF {
-			s.Logger.Fatalf("Failed to read index.html: %s\n", err)
-		}
-
-		html := s.addConfig(buffer)
 
 		w.Write(html)
 	}
 }
 
 func (s *Server) notFound() http.HandlerFunc {
+	file, err := data.Assets.Open("/index.html")
+	if err != nil {
+		s.Logger.Fatalf("Failed to open index.html: %s\n", err)
+	}
+	defer file.Close()
+
+	fileinfo, err := file.Stat()
+	if err != nil {
+		s.Logger.Fatalf("Failed to get file info index.html: %s\n", err)
+	}
+
+	filesize := fileinfo.Size()
+	buffer := make([]byte, filesize)
+
+	_, err = file.Read(buffer)
+	if err != nil && err != io.EOF {
+		s.Logger.Fatalf("Failed to read index.html: %s\n", err)
+	}
+
+	html := s.addConfig(buffer)
+
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/html")
 		w.Header().Set("Vary", "Accept-Encoding")
 		w.WriteHeader(http.StatusNotFound)
-
-		file, err := data.Assets.Open("/index.html")
-		if err != nil {
-			s.Logger.Fatalf("Failed to open index.html: %s\n", err)
-		}
-		defer file.Close()
-
-		fileinfo, err := file.Stat()
-		if err != nil {
-			s.Logger.Fatalf("Failed to get file info index.html: %s\n", err)
-		}
-
-		filesize := fileinfo.Size()
-		buffer := make([]byte, filesize)
-
-		_, err = file.Read(buffer)
-		if err != nil && err != io.EOF {
-			s.Logger.Fatalf("Failed to read index.html: %s\n", err)
-		}
-
-		html := s.addConfig(buffer)
 
 		w.Write(html)
 	}
