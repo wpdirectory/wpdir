78492eb4460f47e6ebf38da1d5db54b1b9879e90
Peter Booker <mail@peterbooker.com>
2018-08-24 14:40:51 +0100

    Updated changelog check to daily, as per Ipstenu feedback

--- internal/repo/repo.go
+++ internal/repo/repo.go
@@ -69,7 +69,7 @@ func New(c *config.Config, l *log.Logger, t string, rev int) *Repo {
 	}
 
 	// Setup Task
-	tasks.Add("0 43 * * * *", repo.jobCheckChangelog)
+	tasks.Add("13 2 * * * *", repo.jobCheckChangelog)
 	tasks.Add("0 2 31 * * *", repo.jobUpdateMeta)
 
 	// Load Existing Data
