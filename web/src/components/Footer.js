90035cd2d621fa30c071b090940a37751c196843
Peter Booker <mail@peterbooker.com>
2018-09-23 20:48:46 +0100

    Add DreamHost link to footer

--- web/src/components/Footer.js
+++ web/src/components/Footer.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import Config from '../utils/Config.js'
+import DreamHostLink from './general/DreamHostLink.js'
 
 const Footer = () => {
   return (
     <footer className="footer cell shrink">
-      <div className="info">&copy; 2018 WP Directory - Made with Love, Go and React by <a href="https://www.peterbooker.com" target="_blank" rel="noopener noreferrer">Peter Booker</a> - <a href="https://github.com/wpdirectory/wpdir" target="_blank" rel="noopener noreferrer" title={'Version: v' + Config.Version + ' Commit: ' + Config.Commit + ' Date: ' + Config.Date}>{ 'v' + Config.Version }</a></div>
+      <div className="info">
+        <span>Made with Love, Go and React by <a href="https://www.peterbooker.com" target="_blank" rel="noopener noreferrer">Peter Booker</a></span>&nbsp;-&nbsp;
+        <span>Powered by <DreamHostLink height="16" width="120" /></span>&nbsp;-&nbsp;
+        <span><a href="https://github.com/wpdirectory/wpdir" target="_blank" rel="noopener noreferrer" title={'Version: v' + Config.Version + ' Commit: ' + Config.Commit + ' Date: ' + Config.Date}>wpdir { Config.Version }</a></span>
+      </div>
     </footer>
   )
 }
